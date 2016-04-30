eb002d0a32a4715103ab2ab41c45002bb4d21658
Andre de Werdt <andredewerdt@hotmail.com>
2016-05-01 00:29:41 +0200

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+console.log('\'Allo \'Allo!');
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
