7c7ad972c3d8743592c104010518083ec01e54c6
Andre de Werdt <andredewerdt@hotmail.com>
2016-05-03 16:47:15 +0200

    overlays added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,57 @@ $(document).on('click', '.navbar-collapse.in', function(e) {
         $(this).collapse('hide');
     }
 });
-// background conversie 
-$(document).ready(function(){
-    $("ul .nav").click(function(){
-        $("li").remove(".active");
-    });
+// background conversie
+// background conversie
+$(document).on('click','ul.nav li',function(){
+        $("li").removeClass("active");
+        $(this).addClass("active");
+        WisselAchtergrondSeizoen($("a",this).attr('id'),'overlay');
 });
+
+function WisselAchtergrondSeizoen(id,klasse) {
+        if(id=="Zomer"){
+          $(klasse+".winter").removeClass('start-overlay');
+          $(klasse+".zomer").addClass('start-overlay');
+        }
+        if(id=="Winter"){
+          $(klasse+".winter").addClass('start-overlay');
+          $(klasse+".zomer").removeClass('start-overlay');
+        }
+
+/*          setTimeout(function(){
+            $(klasse).css('z-index','99');
+          }, 1000);
+          $('body').removeClass('start-overlay');
+          setTimeout(function(){
+            $(klasse).removeClass("winter").addClass("zomer");
+          }, 1000);
+          setTimeout(function(){
+            $('body').addClass('start-overlay');
+          }, 1000);
+          setTimeout(function(){
+            $('overlay').css('z-index','-1');
+          }, 1000);
+        }
+        if(id=='Winter'){
+          $('body').removeClass('start-overlay');
+          setTimeout(function(){
+             $(klasse).removeClass("zomer").addClass("winter");
+          }, 1000);
+          setTimeout(function(){
+            $('body').addClass('start-overlay');
+          }, 1000);
+
+        }
+*/
+/*      if(id=="Zomer"){
+        $('klasse').animate({ opacity: 0 },700,function(){
+        $('klasse').removeClass("winter").addClass("zomer");});
+        $('klasse').animate({ opacity: 1 },700);
+      }
+      if(id=='Winter'){
+        $('klasse').animate({ opacity: 0 },700, function(){
+        $('klasse').removeClass("zomer").addClass("winter");});
+        $('klasse').animate({ opacity: 1 },700);
+      } */
+}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,7 +29,7 @@ gulp.task('scripts', () => {
     .pipe($.sourcemaps.init())
     .pipe($.babel())
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/html'))
+    .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true}));
 });
 
